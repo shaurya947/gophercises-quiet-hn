46f589e35a78b1511430b7f71f5aeab4b24d40ed
shaurya947 <shaurya.arora1@gmail.com>
2023-05-31 21:50:53 -0400

    Add caching

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 	"net/url"
 	"sort"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/shaurya947/gophercises-quiet-hn/hn"
 )
+
+var cache topStoriesCache
 
 func main() {
 	// parse flags
@@ -25,6 +28,7 @@ func main() {
 
 	http.HandleFunc("/", handler(numStories, tpl))
 
+	go updateTopStoriesBackground(numStories)
 	// Start the server
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
@@ -32,11 +36,50 @@ func main() {
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		var client hn.Client
+		var stories []item
+		for {
+			cache.RWMutex.RLock()
+			initialized := cache.initialized
+			err := cache.error
+			stories = make([]item, len(cache.topStories))
+			copy(stories, cache.topStories)
+			cache.RWMutex.RUnlock()
+
+			if !initialized {
+				time.Sleep(100 * time.Millisecond)
+				continue
+			}
+
+			if err != nil && len(stories) == 0 {
+				http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
+				return
+			}
+
+			break
+		}
+
+		data := templateData{
+			Stories: stories,
+			Time:    time.Now().Sub(start),
+		}
+		err := tpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, "Failed to process the template", http.StatusInternalServerError)
+			return
+		}
+	})
+}
+
+func updateTopStoriesBackground(numStories int) {
+	var client hn.Client
+	for {
 		ids, err := client.TopItems()
 		if err != nil {
-			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
-			return
+			cache.RWMutex.Lock()
+			cache.error = err
+			cache.initialized = true
+			cache.RWMutex.Unlock()
+			continue
 		}
 
 		var stories []item
@@ -56,7 +99,7 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 				}
 			default:
 				goroutineCount := wg.GetCount()
-				if i < len(ids) && goroutineCount < numStories {
+				if i < len(ids) && goroutineCount < 10 {
 					wg.Add(1)
 					go fetchStoryItem(&wg, &client, ids[i], storyChan, quitSignal)
 					storyIdToIdx[ids[i]] = i
@@ -71,16 +114,16 @@ func handler(numStories int, tpl *template.Template) http.HandlerFunc {
 		sort.Slice(stories, func(i, j int) bool {
 			return storyIdToIdx[stories[i].ID] < storyIdToIdx[stories[j].ID]
 		})
-		data := templateData{
-			Stories: stories,
-			Time:    time.Now().Sub(start),
-		}
-		err = tpl.Execute(w, data)
-		if err != nil {
-			http.Error(w, "Failed to process the template", http.StatusInternalServerError)
-			return
-		}
-	})
+
+		cache.RWMutex.Lock()
+		cache.initialized = true
+		cache.error = nil
+		cache.topStories = make([]item, len(stories))
+		copy(cache.topStories, stories)
+		cache.RWMutex.Unlock()
+
+		time.Sleep(10 * time.Second)
+	}
 }
 
 func fetchStoryItem(wg *waitGroupWithCount, client *hn.Client, id int, storyChan chan item, quitSignal chan struct{}) {
@@ -113,6 +156,13 @@ func parseHNItem(hnItem hn.Item) item {
 		ret.Host = strings.TrimPrefix(url.Hostname(), "www.")
 	}
 	return ret
+}
+
+type topStoriesCache struct {
+	topStories  []item
+	initialized bool
+	sync.RWMutex
+	error
 }
 
 // item is the same as the hn.Item, but adds the Host field
